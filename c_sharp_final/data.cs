fb26495c74f10f9e6a829351f2c790ad2d28492c
Benson607 <benson940718@gmail.com>
2024-12-12 00:32:11 +0800

    finish in and outcome show and button and month/day data show

---
+++ c_sharp_final/data.cs
@@ -0,0 +1,194 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Globalization;
+
+struct Transaction
+{
+    // 交易時間
+    public TimeSpan Time { get; set; }
+    // 交易描述
+    public string Desc { get; set; }
+    // 交易額
+    public int Amount { get; set; }
+    // 收入或支出
+    public TransactionType Type { get; set; }
+    // 標籤
+    public TransactionTag Tag { get; set; }
+}
+
+enum TransactionType
+{
+    INCOME,
+    OUTCOME
+}
+
+enum TransactionTag
+{
+    FOOD,
+    TRANSPORTATION,
+    ENTERTAINMENT,
+    DAILY,
+    OTHER
+}
+
+class TransactionManager
+{
+    // 當日日期
+    public DateTime Date { get; set; }
+    // 紀錄當日交易
+    public List<Transaction> Transactions { get; set; }
+
+    public TransactionManager()
+    {
+        this.Date = DateTime.Now;
+        this.Transactions = new List<Transaction>();
+    }
+
+    // 排序交易 cmp可選{"time","desc","amount","type","tag"}
+    public static void Sort(List<Transaction> transactions, string cmp)
+    {
+        switch (cmp)
+        {
+            case "time":
+                transactions.Sort((x, y) => x.Time.CompareTo(y.Time));
+                break;
+            case "desc":
+                transactions.Sort((x, y) => x.Desc.CompareTo(y.Desc));
+                break;
+            case "amount":
+                transactions.Sort((x, y) => x.Amount.CompareTo(y.Amount));
+                break;
+            case "type":
+                transactions.Sort((x, y) => x.Type.CompareTo(y.Type));
+                break;
+            case "tag":
+                transactions.Sort((x, y) => x.Tag.CompareTo(y.Tag));
+                break;
+        }
+    }
+
+    // 排序當日交易
+    public void Sort(string cmp) { Sort(this.Transactions, cmp); }
+
+    // 過濾交易 收入/支出
+    public static List<Transaction> Filter(List<Transaction> transactions, TransactionType type)
+    {
+        return transactions.FindAll(t => t.Type == type);
+    }
+
+    // 過濾交易標籤
+    public static List<Transaction> Filter(List<Transaction> transactions, TransactionTag tag)
+    {
+        return transactions.FindAll(t => t.Tag == tag);
+    }
+
+    // 過濾當日交易 收入/支出
+    public List<Transaction> Filter(TransactionType type) { return Filter(this.Transactions, type); }
+    // 過濾當日交易標籤
+
+    public List<Transaction> Filter(TransactionTag tag) { return Filter(this.Transactions, tag); }
+
+
+
+    // 新增交易
+    public void AppendTransaction(Transaction trans)
+    {
+        this.Transactions.Add(trans);
+    }
+
+    public void DeleteTransaction(Transaction trans)
+    {
+        this.Transactions.Remove(trans);
+    }
+
+    static private string LoadDate(string date)
+    {
+        string json;
+
+        if (File.Exists("./data/" + date + ".json"))
+        {
+            json = File.ReadAllText("./data/" + date + ".json");
+        }
+        else
+        {
+            throw new FileNotFoundException($"Cant't not find ./data/{date}.json");
+        }
+
+        return json;
+    }
+
+    public bool SaveDate(DateTime date)
+    {
+        try
+        {
+            string json = JsonConvert.SerializeObject(this.Transactions, Formatting.Indented);
+            File.WriteAllText("./data/" + date.ToString("yyyy_MM_dd") + ".json", json);
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public bool ChangeDate(DateTime date)
+    {
+        if (this.Transactions.Count > 0)
+        {
+            SaveDate(this.Date);
+        }
+
+        this.Date = date;
+
+        try
+        {
+            string json = LoadDate(date.ToString("yyyy_MM_dd"));
+            this.Transactions = JsonConvert.DeserializeObject<List<Transaction>>(json);
+
+            return true;
+        }
+        catch
+        {
+            this.Transactions = new List<Transaction>();
+
+            return false;
+        }
+    }
+
+    // 獲得當月交易
+    static public Dictionary<DateTime, List<Transaction>> GetMonthTransactions(DateTime date)
+    {
+        string prefix = date.ToString("yyyy_MM_");
+        List<string> currentMonthFiles = new List<string>();
+        Dictionary<DateTime, List<Transaction>> monthTrans = new Dictionary<DateTime, List<Transaction>>();
+
+        foreach (string file in Directory.GetFiles("./data/"))
+        {
+            string fileName = Path.GetFileName(file);
+            if (fileName.StartsWith(prefix))
+            {
+                currentMonthFiles.Add(fileName);
+            }
+        }
+
+        foreach (string fileName in currentMonthFiles)
+        {
+            try
+            {
+                string json = File.ReadAllText("./data/" + fileName);
+                List<Transaction> trans = JsonConvert.DeserializeObject<List<Transaction>>(json);
+                DateTime d = DateTime.ParseExact(fileName.Substring(0, fileName.Length - 5), "yyyy_MM_dd", CultureInfo.InvariantCulture);
+                monthTrans.Add(d, trans);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        return monthTrans;
+    }
+}
---
+++ c_sharp_final/main_window.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_sharp_final
+{
+    public partial class main_window : Form
+    {
+
+        private TransactionManager trans_manager;
+
+        private Button[] button_arr;
+        private int show_mode;
+
+        int year_now;
+        int month_now;
+        int day_now;
+
+        int year_choose;
+        int month_choose;
+        int day_choose;
+
+        int total_in;
+        int total_out;
+
+        FormLogin login_window;
+        FormRegister register_window;
+
+        public main_window()
+        {
+            InitializeComponent();
+
+            trans_manager = new TransactionManager();
+
+            login_window = new FormLogin();
+            register_window = new FormRegister();
+
+            login_window.to_register += to_register;
+            login_window.access += access;
+            register_window.to_login += to_login;
+
+            register_window.Show();
+            
+            show_mode = 0;
+            
+            DateTime now = DateTime.Now;
+            year_now = Convert.ToInt32(now.ToString("yyyy"));
+            month_now = Convert.ToInt32(now.ToString("MM"));
+            day_now = Convert.ToInt32(now.ToString("dd"));
+            year_choose = year_now;
+            month_choose = month_now;
+            day_choose = 0;
+
+            year_box.Text = year_now.ToString();
+            month_box.Text = month_now.ToString();
+
+            total_in = 0;
+            total_out = 0;
+            
+            set_day_button();
+            update_button();
+            update_month();
+        }
+
+        private void set_day_button()
+        {
+            button_arr = new Button[42];
+            int margin = 5;
+            int width = 60;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Button button = new Button
+                    {
+                        Text = "",
+                        Width = width,
+                        Height = width,
+                        Left = j * (width + margin) + 5,
+                        Top = i * (width + margin) + 5
+                    };
+                    button.Click += day_button_click;
+                    button_panel.Controls.Add(button);
+                    button_arr[i * 7 + j] = button;
+                }
+            }
+        }
+
+        private void update_button()
+        {
+            day_choose = 0;
+            int[] days_for_month = { 0, 31, 28 + Convert.ToInt32(year_choose % 4 == 0 && year_choose % 100 != 0 || year_choose % 4 == 0 && year_choose % 400 == 0), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int j = 1;
+            int first_day = (int)new DateTime(year_choose, month_choose, 1).DayOfWeek + 1;
+            for (int i = 0; i < 42; i++)
+            {
+                button_arr[i].BackColor = Color.White;
+                if (j >= first_day && j - first_day + 1 <= days_for_month[Convert.ToInt32(month_choose)])
+                {
+                    button_arr[i].Text = (j - first_day + 1).ToString();
+                }
+                else
+                {
+                    button_arr[i].Text = "";
+                }
+                j++;
+            }
+        }
+
+        private void day_button_click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Text == "")
+            {
+                return;
+            }
+            if (day_choose > 0)
+            {
+                button_arr[day_choose + (int)new DateTime(year_choose, month_choose, 1).DayOfWeek - 1].BackColor = Color.White;
+            }
+            day_choose = Convert.ToInt32(button.Text);
+            button_arr[day_choose + (int)new DateTime(year_choose, month_choose, 1).DayOfWeek - 1].BackColor = Color.Red;
+
+            update_day_list();
+        }
+
+        private void update_day_list()
+        {
+            trans_manager.ChangeDate(new DateTime(year_choose, month_choose, day_choose));
+
+            data_list_box.Items.Clear();
+            foreach (var trans in trans_manager.Transactions)
+            {
+                data_list_box.Items.Add($"{trans.Time} - {trans.Desc} - {trans.Amount} - {trans.Tag}");
+            }
+        }
+
+        private void update_month()
+        {
+            Dictionary<DateTime, List<Transaction>> month_trans = TransactionManager.GetMonthTransactions(new DateTime(year_choose, month_choose, 1));
+            month_data_box.Items.Clear();
+            total_in = 0;
+            total_out = 0;
+            foreach (var month_tran in month_trans)
+            {
+                foreach (var trans in month_tran.Value)
+                {
+                    month_data_box.Items.Add($"{trans.Time} - {trans.Desc} - {trans.Amount} - {trans.Tag}");
+                    if (trans.Amount > 0)
+                    {
+                        total_in += trans.Amount;
+                    }
+                    if (trans.Amount < 0)
+                    {
+                        total_out += trans.Amount;
+                    }
+                }
+            }
+            income_text.Text = total_in.ToString();
+            outcome_text.Text = total_out.ToString();
+        }
+
+        private void income_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void month_data_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void day_button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void in_out_switch_Click(object sender, EventArgs e)
+        {
+            show_mode++;
+            if (show_mode == 3)
+            {
+                show_mode = 0;
+            }
+            if (show_mode == 0)
+            {
+                in_out_switch.Text = "顯示條件:不限";
+            }
+            else if (show_mode == 1)
+            {
+                in_out_switch.Text = "顯示條件:僅收入";
+            }
+            else if (show_mode == 2)
+            {
+                in_out_switch.Text = "顯示條件:僅支出";
+            }
+        }
+
+        private void month_up_button_Click(object sender, EventArgs e)
+        {
+            month_choose++;
+            if (month_choose == 13)
+            {
+                month_choose = 1;
+                year_choose++;
+                year_box.Text = year_choose.ToString();
+            }
+            month_box.Text = month_choose.ToString();
+            data_list_box.Items.Clear();
+            update_button();
+            update_month();
+        }
+
+        private void month_down_button_Click(object sender, EventArgs e)
+        {
+            month_choose--;
+            if (month_choose == 0)
+            {
+                month_choose = 12;
+                year_choose--;
+                year_box.Text = year_choose.ToString();
+            }
+            month_box.Text = month_choose.ToString();
+            data_list_box.Items.Clear();
+            update_button();
+            update_month();
+        }
+
+        private void to_login(Object sender, EventArgs e)
+        {
+            register_window.Hide();
+            login_window.Show();
+        }
+
+        private void to_register(Object sender, EventArgs e)
+        {
+            login_window.Hide();
+            register_window.Show();
+        }
+
+        private void access(Object sender, EventArgs e)
+        {
+            login_window.Hide();
+            register_window.Hide();
+            mask.Hide();
+        }
+    }
+}
